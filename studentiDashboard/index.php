38919a09368b5f90700e64172236410b4818c8b2
Erdinand Mujaj <36090421+erdinandmujaj@users.noreply.github.com>
2022-05-07 22:47:09 +0200

    studentiDashboard

---
+++ studentiDashboard/index.php
@@ -0,0 +1,7 @@
+<html><head>
+  <!-- Pjesa e CSS te Material Design -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection">
+  <link href="/css/style.css" type="text/css" rel="stylesheet" media="screen,projection">
+  <title>Dashboard - Studenti</title>
+  </head>
