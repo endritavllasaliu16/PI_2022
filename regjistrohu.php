f24a6041ca39cb83af234981c4e8c877bb0e3cba
Erdinand Mujaj <36090421+erdinandmujaj@users.noreply.github.com>
2022-05-07 22:43:20 +0200

    .

--- regjistrohu.php
+++ regjistrohu.php
@@ -14,3 +14,36 @@
  <center><div class="section no-pad-bot" id="index-banner" style="max-width: 50%;">
     <div class="container">
       <br><br>
+      <div class="card"style="position: relative;padding: 20px 20px 20px 20px;">
+      <center><img src="images/logo.png" width="150px" height="150px"></center>
+      <div class="row">
+    <center><form class="col s12">
+      <div class="row">
+        <div class="input-field col s12">
+          <input type="text" id="first_name" type="text" class="validate">
+          <label for="first_name">Emri</label>
+        </div>
+        <div class="row">
+        <div class="input-field col s12">
+          <input id="last_name" type="text" class="validate">
+          <label for="last_name">Mbiemri</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="username" type="text" class="validate">
+          <label for="email">Username</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="password" type="password" class="validate">
+          <label for="password">Password</label>
+        </div>
+      </div>
+      <button class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></button>
+    </form></center>
+  </div>
+  </div>
+</div>
+</center>
