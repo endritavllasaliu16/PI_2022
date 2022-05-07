371f6991c9f35a06eb894c0b776353c68d5a9359
Erdinand Mujaj <36090421+erdinandmujaj@users.noreply.github.com>
2022-05-07 22:38:11 +0200

    fillojm me login

---
+++ kyqu.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>FIEK - Programimi ne internet</title>
+
+  <!-- Pjesa e CSS te Material Design -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body style="background: #eee;">
+  <center><div class="section no-pad-bot" id="index-banner" style="max-width: 50%;">
+    <div class="container">
+      <br><br>
+      <div class="card"style="position: relative;padding: 20px 20px 20px 20px;">
+      <center><img src="images/logo.png" width="150px" height="150px"></center>
+      <div class="row">
+    <center><form class="col s12">
+        <div class="input-field col s12">
+          <input id="email" type="email" class="validate">
+          <label for="email">Email</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="password" type="password" class="validate">
+          <label for="password">Password</label>
+        </div>
+      </div>
+      <button class="waves-effect waves-light btn red">Kyqu</button>
+      <br>
+      <label>Kliko <a href="/regjistrohu.php">ketu</a> per tu regjistruar</label>
+    </form></center>
+  </div>
+  </div>
+</div></div></center>
+<br>
+<br>
+  <!--  Pjesa e scriptave te Material Design-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+
+  </body>
+</html>
