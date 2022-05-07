3a0955c4733ac5c5f770913efc09e1b196f47153
Erdinand Mujaj <36090421+erdinandmujaj@users.noreply.github.com>
2022-05-07 22:47:58 +0200

    body

--- studentiDashboard/index.php
+++ studentiDashboard/index.php
@@ -5,3 +5,12 @@
   <link href="/css/style.css" type="text/css" rel="stylesheet" media="screen,projection">
   <title>Dashboard - Studenti</title>
   </head>
+  <body style="background: #eee;">
+  <nav>
+    <div class="nav-wrapper red">
+      <img src="../images/logo.png" class="brand-logo" width="50px" height="50px" style="top: 7px;"><a href="#" class="brand-logo" style="left: 55px;font-size: 20px;">emriUsername</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="#">Log out</a></li>
+      </ul>
+    </div>
+  </nav>
