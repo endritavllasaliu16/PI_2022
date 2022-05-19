97fc4f4afa25416e8db244eadb37e6c52ae3d1e1
Erdinand Mujaj <36090421+erdinandmujaj@users.noreply.github.com>
2022-05-20 00:29:28 +0200

    fixed navin tash bon contenti

--- Views/dorezodetyren.view.php
+++ Views/dorezodetyren.view.php
@@ -1,5 +1,10 @@
 <?php require('partials/head.php'); ?>
-
+<body>
  <?php require('partials/nav.php'); ?>
-  
+<center>     
+            <div class="card">
+              <h1>andi</h1>
+            </div>
+</center>
+</body>
 <?php require('partials/footer.php'); ?>
---
+++ Views/partials/navProfesori.php
@@ -0,0 +1,21 @@
+<nav>
+    <div class="nav-wrapper red">
+      <img src="../images/logo.png" class="brand-logo" width="50px" height="50px" style="top: 7px;"><a href="#" class="brand-logo" style="left: 55px;font-size: 20px;">emriUsername</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="#">Log out</a></li>
+      </ul>
+    </div>
+  </nav>
+  <ul id="nav-mobile" class="sidenav" style="transform: translateX(0px);position: inherit;box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);padding-bottom: 100%;">
+        <li class="logo"></li>
+        
+      <li class="no-padding">
+        <ul class="collapsible collapsible-accordion">
+ <li class="bold"><a class="collapsible-header waves-effect waves-teal" href="student" tabindex="0">Home</a></li>
+ <li class="bold"><a class="collapsible-header waves-effect waves-teal" href="lendet" tabindex="0">Lendet</a></li>
+ <li class="bold"><a class="collapsible-header waves-effect waves-teal" href="detyratemia" tabindex="0">Dorezimet</a></li>
+ <li class="bold"><a class="collapsible-header waves-effect waves-teal" href="vleresimet" tabindex="0">Vleresimet</a></li>
+ <li class="bold"><a class="collapsible-header waves-effect waves-teal" href="dorezodetyren" tabindex="0">FAQ</a></li>        
+</ul>
+      </li>
+</ul>
