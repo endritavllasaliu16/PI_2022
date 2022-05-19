c3e2b393d3501351169a358e65bdafc1258073b7
Erdinand Mujaj <36090421+erdinandmujaj@users.noreply.github.com>
2022-05-19 22:11:54 +0200

    cdn

--- Views/index.view.php
+++ Views/index.view.php
@@ -6,15 +6,16 @@
   <title>FIEK - Programimi ne internet</title>
   <!-- Pjesa e CSS te Material Design -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link href="C:\xampp\htdocs\PI_2022\assets\css\materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  <link href="C:\xampp\htdocs\PI_2022\assets\css\style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <!-- <link href="C:\xampp\htdocs\PI_2022\assets\css\materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="C:\xampp\htdocs\PI_2022\assets\css\style.css" type="text/css" rel="stylesheet" media="screen,projection"/> -->
 </head>
 <body style="background: #eee;">
   <center><div class="section no-pad-bot" id="index-banner" style="max-width: 50%;">
     <div class="container">
       <br><br>
       <div class="card"style="position: relative;padding: 20px 20px 20px 20px;">
-      <center><img src="../assets/images/logo.png" width="150px" height="150px"></center>
+      <center><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/University_of_Prishtina_logo.svg/2053px-University_of_Prishtina_logo.svg.png" width="150px" height="150px"></center>
       <div class="row">
     <center><form class="col s12">
         <div class="input-field col s12">
@@ -39,7 +40,8 @@
 <br>
   <!--  Pjesa e scriptave te Material Design-->
   <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-  <script src="C:\xampp\htdocs\PI_2022\assets\js\materialize.js"></script>
-  <script src="C:\xampp\htdocs\PI_2022\assets\js\init.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<!--   <script src="C:\xampp\htdocs\PI_2022\assets\js\materialize.js"></script>
+  <script src="C:\xampp\htdocs\PI_2022\assets\js\init.js"></script> -->
   </body>
 </html>
