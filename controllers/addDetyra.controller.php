dd3a4aab1b213f7f62574c4ecaf0d76b1ce8e173
fletamujaj <fleta.mujaj@student.uni-pr.edu>
2022-05-20 22:57:10 +0200

    Results

--- controllers/addDetyra.controller.php
+++ controllers/addDetyra.controller.php
@@ -2,3 +2,18 @@
 
 
 require 'views/addDetyra.view.php';
+
+
+
+if ( isset( $_POST[ 'submit' ] ) ){
+$app['database']->insert('detyrat',[
+	'ID_detyra'=>$_POST['IDdetyra'],
+	'titulli'=>$_POST['EmriDetyres'],
+	'pershkrimi'=>$_POST['PershkrimiDetyres'],
+	'deadline'=>$_POST['Deadline'],
+	'lenda'=>$_POST['Lenda']
+]);
+};
+
+?>
+
---
+++ controllers/detyratemia.controller copy.php
@@ -0,0 +1,4 @@
+<?php
+
+
+require 'views/detyratemia.view.php';
