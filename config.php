91d257e80bab21389d032edc8cfe43ba161397f8
fletamujaj <fleta.mujaj@student.uni-pr.edu>
2022-05-21 19:11:30 +0200

    config

--- config.php
+++ config.php
@@ -4,8 +4,8 @@ return [
     'database' => [
         'name' => 'projekti',
         'username' => 'root',
-        'password' => '',
-        'connection' => 'mysql:host=127.0.0.1',
+        'password' => 'root',
+        'connection' => 'mysql:host=127.0.0.1:3308',
         'options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         ]
