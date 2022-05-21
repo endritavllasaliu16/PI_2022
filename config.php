416969909c62df9edb52e65e03cf3020a4ffde5b
Dreni Mujaj <59891753+DreniMujaj@users.noreply.github.com>
2022-05-21 17:26:03 +0200

    adding backend validation for register form

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ return [
     'database' => [
         'name' => 'projekti',
         'username' => 'root',
-        'password' => 'root',
+        'password' => '',
         'connection' => 'mysql:host=127.0.0.1',
         'options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
