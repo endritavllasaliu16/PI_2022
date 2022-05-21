cf2e2bb7ae6341c835a5dcbc550a9ff8e74847df
Erdison Berisha <79603797+erdisonberisha@users.noreply.github.com>
2022-05-21 17:49:08 +0200

    Autofill autoincrement fixed

--- Views/shtoLenden.view.php
+++ Views/shtoLenden.view.php
@@ -2,6 +2,22 @@
 
  <?php require('partials/navProfesori.php'); ?>
 
+ <?php
+$conn = mysqli_connect("localhost", "root", "root", "projekti");
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+$sql = "SELECT ID_lenda, emri, kredi, semestri FROM lendet";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+// output data of each row
+while($row = $result->fetch_assoc()) {
+  $ID_lenda = $row['ID_lenda'] + 1;
+}
+}
+  ?>
+
  <body>
  <center><div class="section no-pad-bot" id="index-banner" style="max-width: 50%;">
     <div class="container">
@@ -11,8 +27,8 @@
     <center><form action="saveLenda.view.php" method="post" class="col s12">
     <div class="row">
         <div class="input-field col s12">
-          <input name="ID_lenda" id="lenda_id" type="number" class="validate" min="1" required>
-          <label for="lenda_id">ID e Lendes</label>
+          <input name="ID_lenda" id="lenda_id" type="number" class="validate" value="<?php echo $ID_lenda; ?>" readonly>
+          <label for="lenda_id"></label>
         </div>
       </div>
       <div class="row">
@@ -28,7 +44,7 @@
       </div>
       <div class="row">
         <div class="input-field col s12">
-          <input name="semestri" id="semestri" type="number" class="validate" min="1" max="2" required>
+          <input name="semestri" id="semestri" type="number" class="validate" min="1" max="6" required>
           <label for="semestri">Semestri</label>
         </div>
       </div>
