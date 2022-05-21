8fe266553acf727e739e74241042838ac9832e65
Erdison Berisha <79603797+erdisonberisha@users.noreply.github.com>
2022-05-21 11:00:02 +0200

    Ruajtja e lendeve ne databaz.

--- Views/lendet.view.php
+++ Views/lendet.view.php
@@ -1,6 +1,6 @@
 <?php require('partials/head.php'); ?>
 
- <?php require('partials/nav.php'); ?>
+ <?php require('partials/navProfesori.php'); ?>
 
  <body>
  <center><div class="section no-pad-bot" id="index-banner" style="max-width: 50%;">
@@ -8,27 +8,27 @@
       <br><br>
       <div class="card"style="position: relative;padding: 20px 20px 20px 20px; left: 150px;">
       <div class="row">
-    <center><form class="col s12">
+    <center><form action="saveLenda.php" method="post" class="col s12">
     <div class="row">
         <div class="input-field col s12">
-          <input id="lenda_id" type="number" class="validate" min="1">
+          <input name="ID_lenda" id="lenda_id" type="number" class="validate" min="1">
           <label for="lenda_id">ID e Lendes</label>
         </div>
       </div>
       <div class="row">
         <div class="input-field col s12">
-          <input type="text" id="subject_name" type="text" class="validate">
+          <input name="emri" type="text" id="subject_name" type="text" class="validate">
           <label for="subject_name">Emri i lendes</label>
         </div>
       <div class="row">
         <div class="input-field col s12">
-          <input id="credits" type="number" class="validate" min="3" max="8">
+          <input name="kredi" id="credits" type="number" class="validate" min="3" max="8">
           <label for="credits">Kredite e lendes</label>
         </div>
       </div>
       <div class="row">
         <div class="input-field col s12">
-          <input id="semestri" type="number" class="validate" min="1" max="2">
+          <input name="semestri" id="semestri" type="number" class="validate" min="1" max="2">
           <label for="semestri">Semestri</label>
         </div>
       </div>
@@ -40,5 +40,6 @@
 </center>
 <br>
 <br>
+</body>
   
 <?php require('partials/footer.php'); ?>
