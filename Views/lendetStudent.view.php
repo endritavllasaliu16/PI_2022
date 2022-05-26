a6b06d9b591823d2e18c3c76bce844f3bb4aef06
Endrita Vllasaliu <endrita.vllasaliu@student.uni-pr.edu>
2022-05-26 23:04:53 +0200

    Add lendetStudent view

--- Views/lendetStudent.view.php
+++ Views/lendetStudent.view.php
@@ -1,39 +1,28 @@
 <?php require('partials/head.php'); ?>
 
- <?php require('partials/nav.php'); ?>
+ <?php require('partials/navProfesori.php'); ?>
+ <script language="javascript">
+    document.title = "Lendet";
+</script>
  <body>
- <center>
-<div class="section no-pad-bot" id="index-banner" style="max-width: 50%;">
-<div class="container"  style="position: relative;padding: 20px 20px 20px 20px;">
-<div class="row card">
-		<center><label>Lendet e regjistruara nga fakulteti</label></center>
-	<br>
-	<br>
-<table style="padding: 20px 20px 20px 20px;">
+ <center><div class="section no-pad-bot" id="index-banner" style="max-width: 50%;">
+ <div class="container"  style="position: relative;padding: 20px 20px 20px 20px;">
+	<div class="row">
+		<div class="col-md-12 text-center">
+          <a href="shtoLenden"><button  href="" type="submit" class="btn btn-primary" style="border-radius:10%">Shto Lenden</button></a>
+		</div>
+	</div>
+</div>
+<br>
+<br>
+<div class="row">
+<table>
 <tr>
 <th>Id e Lendes</th>
-<th>Emri</th>
+<th>Lenda</th>
 <th>Kredite</th>
 <th>Semestri</th>
 </tr>
-<?php
-$conn = mysqli_connect("localhost", "root", "root", "projekti");
-if ($conn->connect_error) {
-die("Lidhja u shkput: " . $conn->connect_error);
-}
-$sql = "SELECT ID_lenda, emri, kredi, semestri FROM lendet";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-while($row = $result->fetch_assoc()) {
-	echo "<tr><td>" . $row["ID_lenda"]. "</td><td>" . $row["emri"] . "</td><td>"
-	. $row["kredi"]. "</td><td>" . $row["semestri"]. "</td></tr>";
-}
-echo "</table>";
-} else { echo "0 results"; }
-$conn->close();
-?>
-</div>
-</div>
 </div>
 </center>
 </body>
