ac62033f5913dc5d5c121eb34d3853fc6d6c4675
Erdison Berisha <79603797+erdisonberisha@users.noreply.github.com>
2022-05-29 12:33:50 +0200

    .

--- controllers/addDetyra.controller.php
+++ controllers/addDetyra.controller.php
@@ -21,14 +21,12 @@ if($user_data[0]->roli=="student"){
 //          . mysqli_connect_error());
 //  }
 if($_SERVER['REQUEST_METHOD'] == "POST"){
- $ID_detyra =  $_REQUEST['IDdetyra'];
  $titulli = $_REQUEST['titulli'];
  $pershkrimi =  $_REQUEST['pershkrimi'];
  $deadline = $_REQUEST['deadline'];
  $lenda = $_REQUEST['lenda'];
   
  $app['database']->insert("detyrat",[
-            "ID_detyra"=>$ID_detyra,
             "titulli"=> $titulli,
             "pershkrimi"=>$pershkrimi,
             "deadline"=> $deadline,
@@ -41,26 +39,14 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 // $result = $mysqli->query($sql);
 // if ($result->num_rows > 0) {
 // // output data of each row
-// while($row = $result->fetch_assoc()) {
+// while() {
 //   $ID_detyra = $row['ID_detyra'] + 1;
 // }
 // }
 // else{
 //   $ID_detyra = 1;
 // }
-//  $sql = "INSERT INTO detyrat  VALUES ('$ID_detyra',
-//      '$titulli','$pershkrimi','$deadline','$lenda')";
-  
 
- //if(mysqli_query($connect, $sql)){
-//  echo "<h4>Detyra u ruajt me sukses!</h4>";
-// } else{
-//     echo "ERROR: Hush! Sorry $sql. "
-//         . mysqli_error($connect);
-// }
-
-  
- //mysqli_close($connect);
 require 'views/addDetyra.view.php';
  ?>
 
