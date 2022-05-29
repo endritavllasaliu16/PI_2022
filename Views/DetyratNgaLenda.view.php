9f9f0dc4dafe712c4041221313dbe788b835a0bf
Endrita Vllasaliu <endrita.vllasaliu@student.uni-pr.edu>
2022-05-29 19:22:36 +0200

    .

--- Views/DetyratNgaLenda.view.php
+++ Views/DetyratNgaLenda.view.php
@@ -8,14 +8,15 @@
 <br>
 <br>
 <div class="row">
-<table id="myTable" style="padding-top:25px;">
+<table style="padding-top:25px;">
  <h6>Detyrat ne <?php echo $emri?></h6>
 <tr>
 <th>Pershkrimi</th>
 <th>Titulli</th>
 <th>Data e dorezimit</th>
 </tr>
-</table>
+
+
 <?php
 foreach($result as $value){
 	?>
@@ -33,14 +34,6 @@ foreach($result as $value){
 <?php
 }	
 ?>
-</div>
-<div class="section no-pad-bot" id="index-banner" style="max-width: 50%;">
- <div class="container"  style="position: relative;padding: 50px 20px 20px 20px;">
-	<div class="row">
-		<div class="col-md-12 text-center">
-          <a href="lendetStudent"><button  href="" type="submit" class="btn btn-primary" style="border-radius:10%">Kthehu tek lendet</button></a>
-		</div>
-	</div>
 </div>
 </center>
 </body>
