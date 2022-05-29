85475d44845a1ac1dd46a90a67d5a24db001d399
Dreni Mujaj <59891753+DreniMujaj@users.noreply.github.com>
2022-05-30 00:35:35 +0200

    final

--- controllers/connect.php
+++ controllers/connect.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = mysqli_connect("localhost:3308", "root", "root", "projekti");
+$mysqli = mysqli_connect("localhost", "root", "", "projekti");
 if ($mysqli->connect_error) {
 die("Connection failed: " . $mysqli->connect_error);
 }
