56ba7d140cca6acd482dfe182287ccda85fadd24
fletamujaj <fleta.mujaj@student.uni-pr.edu>
2022-05-30 00:09:36 +0200

    commit

--- controllers/connect.php
+++ controllers/connect.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = mysqli_connect("localhost:3307", "root", "", "projekti");
+$mysqli = mysqli_connect("localhost:3308", "root", "root", "projekti");
 if ($mysqli->connect_error) {
 die("Connection failed: " . $mysqli->connect_error);
 }
